2fa43e9a4b3cbd3b11a920afed54bdb5e471cf9a
AlexAvalosxP <AlexAvalosxP@users.noreply.github.com>
2018-09-28 15:09:54 -0500

    Initial commit

---
+++ Herramientas/js/funciones.js
@@ -0,0 +1,178 @@
+flagPareto = false;
+
+function display(opt)
+{
+	midArea = document.getElementById("midArea");
+	switch (opt) 
+	{
+		case 1:
+			midArea.innerHTML = '<div class="infoArea">' +
+			'<h1>Diagrama de Ishikawa</h1>' +
+			'<p>Consiste en una representación gráfica que permite visualizar las causas que explican un determinado problema, lo cual la convierte en una herramienta de la Gestión de la Calidad ampliamente utilizada dado que orienta la toma de decisiones al abordar las bases que determinan un desempeño deficiente.</p>' +
+			'<p>Identifica un problema o efecto y luego enumera un conjunto de causas que potencialmente explican dicho comportamiento. Adicionalmente cada causa se puede desagregar con grado mayor de detalle en subcausas.</p>' +
+			'</div>' +
+			'<div class="interactArea" id="interactArea">' +		
+			'</div>';
+			flagPareto = false;
+		break;
+		case 2:
+			midArea.innerHTML = '<div class="infoArea">' +
+			'<h1>Diagrama de Pareto</h1>' +
+			'<p>Es una gráfica para organizar datos de forma que estos queden en orden descendente, de izquierda a derecha y separados por barras.</p>' +
+			'<p>Con el Diagrama de Pareto se pueden detectar los problemas que tienen más relevancia mediante la aplicación del principio de Pareto (pocos vitales, muchos triviales) que dice que hay muchos problemas sin importancia frente a solo unos graves. Ya que por lo general, el 80% de los resultados totales se originan en el 20% de los elementos.</p>' +
+			'</div>' +
+			'<div class="interactArea" id="interactArea"><div id="chart_div" style="width: 900px; height: 500px;"></div>' +		
+			'</div>';
+			flagPareto = true;
+			drawVisualization();
+		break;
+		case 3:
+			midArea.innerHTML = '<div class="infoArea">' +
+			'<h1>6 Sombreros para pensar</h1>' +
+			'<p>Sombrero blanco: Este sombrero nos enseñará a ver las cosas desde un punto de vista objetivo, neutro y vacío de sesgos.</p>' +
+			'<p>Sombrero negro: Representa lo lógico-negativo y nos enseña a comprender por qué ciertas cosas pueden salir mal, no funcionar o no acontecer de la forma que nosotros pensamos.</p>' +
+			'<p>Sombrero rojo: Es pasional, es emotivo y siente la vida desde el corazón y el universo emocional.</p>' +
+			'<p>Sombrero amarillo: Nos enseña a aplicar un enfoque de pensamiento lógico-positivo.</p>' +
+			'<p>Sombrero verde: Se contiene el pensamiento lateral, ese que nos invita a ser provocativos y no tan conservadores, a usar el movimiento novedoso antes que el juicio restrictivo.</p>' +
+			'<p>Sombrero azul: Representa por tanto el pensamiento estructurado, el que se centra y nos guía en cada paso, señalando alternativas, proponiendo nuevas estrategias .</p>' +
+			'</div>' +
+			'<div class="interactArea" id="interactArea">' +		
+			'</div>';
+			flagPareto = false;
+		break;
+		default:
+			midArea.innerHTML = 'ERROR';
+			flagPareto = false;
+		break;
+	}
+}
+
+function applySettings()
+{
+	setArea = document.getElementById("settingsArea");
+	setArea.hidden = true;
+	casesName = document.getElementsByName("caseName");
+	casesValue = document.getElementsByName("caseVal");
+	drawVisualization();
+	/*
+	for (i = 0; i < casesName.length; i++) {
+		console.log(casesName[i].value);
+		console.log(casesValue[i].value);
+	}
+	*/
+}
+
+function changeSettings()
+{
+	setArea = document.getElementById("settingsArea");
+	setArea.hidden = false;
+}
+
+caseX = 1;
+
+function addSpace()
+{
+	caseX = caseX + 1;
+	cases = document.getElementById("cases");
+
+	newDiv = document.createElement('div');
+	newDiv.id = "case" + caseX;
+
+	newName = document.createElement('input');
+	newName.setAttribute('type', 'text');
+	newName.setAttribute('name', 'caseName');
+
+	newVal = document.createElement('input');
+	newVal.setAttribute('type', 'number');
+	newVal.setAttribute('name', 'caseVal');
+
+	newBtnPlus = document.createElement('button');
+	newBtnPlus.innerHTML = "+";
+	newBtnPlus.setAttribute('onclick', 'addSpace()');
+
+	newBtnLess = document.createElement('button');
+	newBtnLess.innerHTML = "-";
+	newBtnLess.setAttribute('onclick', 'deleteSpace(this.parentNode.id)');
+
+	newDiv.appendChild(newName);
+	newDiv.appendChild(newVal);
+	newDiv.appendChild(newBtnPlus);
+	newDiv.appendChild(newBtnLess);
+	cases.appendChild(newDiv);
+}
+
+function deleteSpace(id)
+{
+	caseT = document.getElementById(id);
+	caseT.parentElement.removeChild(caseT);
+}
+
+function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+
+    if (flagPareto)
+    {
+
+    var data = google.visualization.arrayToDataTable(getValues());
+
+    var options = {
+      title : 'Diagrama de pareto',
+      vAxis: {title: 'Valor'},
+      hAxis: {title: 'Problemas'},
+      seriesType: 'bars',
+      series: {1: {type: 'line'}},
+      height: document.getElementById('interactArea').clientHeight,
+      width: parseInt(document.getElementById('interactArea').clientWidth) - 50,
+      titleTextStyle: {
+        color: 'white'
+    },
+    hAxis: {
+        textStyle: {
+            color: 'white'
+        },
+        titleTextStyle: {
+            color: 'white'
+        }
+    },
+    vAxis: {
+        textStyle: {
+            color: 'white'
+        },
+        titleTextStyle: {
+            color: 'white'
+        }
+    },
+    legend: {
+        textStyle: {
+            color: 'white'
+        }
+    }
+    };
+
+    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+function getValues()
+{
+	valueArray = [];
+
+	valueArray.push(["x", "Frec. Rel", "Frec. Acum"]);
+
+	total = 0;
+
+	for (i = 0; i < casesName.length; i++) {
+		total += parseInt(casesValue[i].value);
+	}
+
+	sum = 0;
+	for (i = 0; i < casesName.length; i++) {
+		act = parseInt(casesValue[i].value)/total
+		sum += act;
+		valueArray.push([String(casesName[i].value), act, sum]);
+	}
+
+	console.log(valueArray);
+	return valueArray;
+}
+}
